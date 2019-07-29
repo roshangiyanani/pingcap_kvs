cfdf7ffe6f2850ab4f5000640eef7fc74c46741d
Roshan Giyanani <roshangiyanani@gmail.com>
2019-07-28 20:00:35 -0400

    use StructOpt to manage clap

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,100 +1,45 @@
-use clap::{App, AppSettings, Arg, SubCommand};
-use strum_macros::{Display, EnumString};
-
-// use kvs::KvStore;
+// use clap::{App, AppSettings, Arg, SubCommand};
+use structopt::StructOpt;
+use strum_macros::Display;
 
 fn main() {
-    let mode = get_mode();
-    println!("mode: {:?}", mode);
+    let command = Command::from_args();
+    println!("command: {:?}", command);
 
     eprintln!("unimplemented");
     unimplemented!();
 
     // let mut kvs = KvStore::new();
 
-    // match mode {
-    //     Mode::Get(key) => {
+    // match command {
+    //     Command::Get{ key } => {
     //         kvs.get(key);
     //     }
-    //     Mode::Set(key, value) => kvs.set(key, value),
-    //     Mode::Remove(key) => kvs.remove(key),
+    //     Command::Set{ key, value } => kvs.set(key, value),
+    //     Command::Remove{ key } => kvs.remove(key),
     // }
 }
 
-fn get_mode() -> Mode {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .setting(AppSettings::VersionlessSubcommands)
-        .subcommand(
-            SubCommand::with_name("get")
-                .about("retreive a value from the key-value store")
-                .arg(
-                    Arg::with_name("key")
-                        .help("the item to retreive the value of")
-                        .required(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("set")
-                .about("add a value to the key-value store")
-                .arg(
-                    Arg::with_name("key")
-                        .help("the name to store the value under")
-                        .required(true),
-                )
-                .arg(
-                    Arg::with_name("value")
-                        .help("the value to store")
-                        .required(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("rm")
-                .about("remove a value from the key-value store")
-                .arg(
-                    Arg::with_name("key")
-                        .help("the item to delete")
-                        .required(true),
-                ),
-        )
-        .get_matches();
-
-    match matches.subcommand() {
-        ("get", Some(mode_matches)) => Mode::Get(String::from(
-            mode_matches
-                .value_of("key")
-                .expect("subcommand get has no argument 'key'"),
-        )),
-        ("set", Some(mode_matches)) => Mode::Set(
-            String::from(
-                mode_matches
-                    .value_of("key")
-                    .expect("subcommand set has no argument 'key'"),
-            ),
-            String::from(
-                mode_matches
-                    .value_of("value")
-                    .expect("subcommand set has no argument 'value'"),
-            ),
-        ),
-        ("rm", Some(mode_matches)) => Mode::Remove(String::from(
-            mode_matches
-                .value_of("key")
-                .expect("subcommand rm has no argument 'key'"),
-        )),
-        _ => unreachable!(), // one subcommand is required
-    }
-}
-
-#[derive(Debug, Display, EnumString)]
-enum Mode {
-    #[strum(serialize = "get", serialize = "g")]
-    Get(String),
-    #[strum(serialize = "set", serialize = "s")]
-    Set(String, String),
-    #[strum(serialize = "remove", serialize = "rm", serialize = "r")]
-    Remove(String),
+#[derive(Debug, Display, StructOpt)]
+enum Command {
+    #[structopt(name = "get")]
+    /// Retrieve a value from the key-value store.
+    Get {
+        /// The item to retreive the value of.
+        key: String,
+    },
+    #[structopt(name = "set")]
+    /// Add a value to the key-value store.
+    Set {
+        /// The name to store the value under.
+        key: String,
+        /// The value to store.
+        value: String,
+    },
+    #[structopt(name = "rm")]
+    /// Remove a value from the key-value store.
+    Remove {
+        /// The item to delete.
+        key: String,
+    },
 }
